0e7d7e9f045e4d1f90f7953e1baa6a6d32df7610
Som Jadhav <somjadhav787@gmail.com>
2021-03-14 15:59:27 -0500

    first commit

---
+++ src/data/SidebarData.js
@@ -0,0 +1,55 @@
+import MessageIcon from '@material-ui/icons/Message';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleIcon from '@material-ui/icons/People';
+import AppsIcon from '@material-ui/icons/Apps';
+
+
+export const sidebarItemsData = [
+    {
+        icon: <MessageIcon />,
+        text: "Thread"
+    },
+    {
+        icon: <InboxIcon />,
+        text: "All DMs"
+    },
+    {
+        icon: <DraftsIcon />,
+        text: "Mentions & Reactions"
+    },
+    {
+        icon: <BookmarkBorderIcon />,
+        text: "Saved Items"
+    },
+    {
+        icon: <PeopleIcon />,
+        text: "Peoples & Groups"
+    },
+    {
+        icon: <AppsIcon />,
+        text: "More"
+    }
+]
+
+export const sidebarChannels = [
+    {
+        text: "# Channel 1"
+    },
+    {
+        text: "# general"
+    },
+    {
+        text: "# Channel 2"
+    },
+    {
+        text: "# random"
+    },
+    {
+        text: "# Channel 3"
+    },
+    {
+        text: "# officers"
+    },
+]
